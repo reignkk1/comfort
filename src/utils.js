f99a00168ff56771cb540bd4adeadf119f0dfa7a
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-07-31 17:39:08 +0900

    feat: className 리턴 함수

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,8 @@ export function createElement(tag, attr) {
     for (const [prop, value] of Object.entries(attr)) {
       if (prop === "innerText") {
         element.innerText = value;
+      } else if (prop === "append") {
+        element.append(value[0]);
       } else {
         element.setAttribute(prop, value);
       }
@@ -13,4 +15,24 @@ export function createElement(tag, attr) {
   }
 
   return element;
+}
+
+// 아이콘 ClassName 리턴함수
+export function getIconClassName(icon) {
+  let className;
+
+  if (icon === "pause") {
+    className = "fas fa-pause fa-lg";
+  } else if (icon === "play") {
+    className = "fas fa-play fa-lg";
+  } else if (icon === "muted") {
+    className = "fas fa-volume-mute fa-lg";
+  } else if (icon === "volume") {
+    if (volumeState > 0.5) {
+      className = "fas fa-volume-up fa-lg";
+    } else {
+      className = "fas fa-volume-down fa-lg";
+    }
+  }
+  return className;
 }
