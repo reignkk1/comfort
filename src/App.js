283b9e6b080a78998dacee9cb4789cfdfb360dc0
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-07-31 18:27:03 +0900

    fix: import name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { createElement } from "./utils.js";
 import commentsContainer from "./Comments.js";
-import musicScreen from "./Audio.js";
+import audioScreen from "./Audio.js";
 import "./reset.css";
 import "./styles.css";
 
 const app = createElement("main", { class: "main-content" });
 
-app.append(musicScreen, commentsContainer);
+app.append(audioScreen, commentsContainer);
 
 export default app;
