00514217655ce9e21c29493054ace4d0ee3632bc
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-08-09 15:58:57 +0900

    refactor: 댓글 클래스 모듈

--- src/Comments.js
+++ src/Comments.js
@@ -1,173 +1,206 @@
-import { createElement } from "./utils.js";
+import { createComment, createElement, getElement } from "./utils.js";
 
-const commentsContainer = createElement("section", { class: "comments" });
+class LoginForm {
+  constructor() {
+    this.loginForm = createElement("div", { class: "input-container" });
+    this.nicknameContainer = createElement("div");
+    this.nicknameLabel = createElement("span", {
+      class: "nickname-label",
+      innerText: "닉네임: ",
+    });
+    this.nicknameInput = createElement("input", {
+      class: "nickname-input",
+      type: "text",
+      maxlength: "20",
+    });
 
-const commentWrite = createElement("div", { class: "comment-write" });
-const commentsList = createElement("ul", { class: "comments-list" });
+    this.passWordContainer = createElement("div");
+    this.passWordLabel = createElement("span", {
+      class: "passWord-label",
+      innerText: "비밀번호: ",
+    });
+    this.passWordInput = createElement("input", {
+      class: "passWord-input",
+      type: "password",
+      maxlength: "6",
+    });
+  }
 
-const { buttonsContainer, cancelButton, submitButton } = createButtonsComment();
-const { textareaContainer, textarea } = createTextAreaComment();
-const { inputContainer, nicknameInput, passWordInput } = createInputs();
+  init() {
+    this.renderLoginForm();
+    this.addEventListenerLoginForm();
+    return this.loginForm;
+  }
 
-renderInputs();
-renderCommentWrite();
-renderCommentsList();
-addEventListeners();
+  addEventListenerLoginForm() {
+    const { nicknameInput, passWordInput } = this;
 
-// input 렌더링
-function renderInputs() {
-  commentsContainer.appendChild(inputContainer);
-}
+    nicknameInput.oninput = () => {
+      nickName = nicknameInput.value;
+    };
+    passWordInput.oninput = () => {
+      passWord = passWordInput.value;
+    };
+  }
 
-// 댓글 쓰기 렌더링
-function renderCommentWrite() {
-  commentWrite.append(textareaContainer, buttonsContainer);
-  commentsContainer.appendChild(commentWrite);
-}
+  renderLoginForm() {
+    const {
+      nicknameContainer,
+      nicknameInput,
+      nicknameLabel,
+      passWordContainer,
+      passWordInput,
+      passWordLabel,
+      loginForm,
+    } = this;
 
-// 초기 댓글 리스트 렌더링 => ajax
-function renderCommentsList() {
-  createComment("김민겸", "노래가 너무 좋습니다!");
-}
-
-// 이벤트 리스너들
-function addEventListeners() {
-  textarea.addEventListener("focus", () => {
-    buttonsContainer.classList.remove("no-show");
-  });
-
-  textarea.oninput = () => {
-    textarea.style.height = "auto";
-    textarea.style.height = textarea.scrollHeight + 1 + "px";
-
-    if (textarea.value) {
-      activeSubmitButton(true);
-    } else {
-      activeSubmitButton(false);
-    }
-  };
-
-  cancelButton.addEventListener("click", () => {
-    textarea.value = "";
-    buttonsContainer.classList.add("no-show");
-    activeSubmitButton(false);
-  });
-
-  submitButton.addEventListener("click", () => {
-    if (!nicknameInput.value || !passWordInput.value) {
-      return alert("닉네임 또는 비밀번호를 입력해주세요!");
-    }
-
-    // 초기화 로직 짜기 따로 모듈로
-    createComment(nicknameInput.value, textarea.value);
-    nicknameInput.value = "";
-    passWordInput.value = "";
-
-    textarea.value = "";
-    textarea.style.height = "auto";
-    activeSubmitButton(false);
-  });
-}
-
-// 등록 버튼 활성화
-function activeSubmitButton(active) {
-  if (active) {
-    submitButton.classList.remove("submit-disabled");
-    submitButton.classList.add("submit-active");
-    submitButton.disabled = false;
-  } else {
-    submitButton.classList.remove("submit-active");
-    submitButton.classList.add("submit-disabled");
-    submitButton.disabled = true;
+    nicknameContainer.append(nicknameLabel, nicknameInput);
+    passWordContainer.append(passWordLabel, passWordInput);
+    loginForm.append(nicknameContainer, passWordContainer);
   }
 }
 
-// 닉네임, 비밀번호 input 생성
-function createInputs() {
-  const inputContainer = createElement("div", { class: "input-container" });
-  const nicknameContainer = createElement("div");
-  const nicknameLabel = createElement("span", {
-    class: "nickname-label",
-    innerText: "닉네임: ",
-  });
-  const nicknameInput = createElement("input", {
-    class: "nickname-input",
-    type: "text",
-    maxlength: "20",
-  });
+class CommentList {
+  constructor() {
+    this.commentsList = createElement("ul", { class: "comments-list" });
+  }
 
-  const passWordContainer = createElement("div");
-  const passWordLabel = createElement("span", {
-    class: "passWord-label",
-    innerText: "비밀번호: ",
-  });
-  const passWordInput = createElement("input", {
-    class: "passWord-input",
-    type: "password",
-    maxlength: "6",
-  });
+  init() {
+    this.renderCommentList();
+    return this.commentsList;
+  }
 
-  nicknameContainer.append(nicknameLabel, nicknameInput);
-  passWordContainer.append(passWordLabel, passWordInput);
-
-  inputContainer.append(nicknameContainer, passWordContainer);
-
-  return {
-    inputContainer,
-    nicknameInput,
-    passWordInput,
-  };
+  renderCommentList() {
+    // 댓글 리스트 불러오기 => ajax
+    createComment(this.commentsList, "김민겸", "노래가 너무 좋습니다!");
+    createComment(this.commentsList, "김민겸", "노래가 너!");
+  }
 }
 
-// 댓글 쓰는 TextArea 생성
-function createTextAreaComment() {
-  const textareaContainer = createElement("div", {
-    class: "textarea-container",
-  });
-  const textarea = createElement("textarea", {
-    class: "textarea",
-    placeholder: "댓글 추가..",
-    rows: "1",
-  });
+class CommentWrite {
+  constructor() {
+    this.commentWrite = createElement("div", { class: "comment-write" });
+    this.textareaContainer = createElement("div", {
+      class: "textarea-container",
+    });
+    this.textarea = createElement("textarea", {
+      class: "textarea",
+      placeholder: "댓글 추가..",
+      rows: "1",
+    });
+    this.buttonsContainer = createElement("div", {
+      class: "buttons-container no-show",
+    });
+    this.cancelButton = createElement("button", {
+      class: "button-cancel",
+      innerText: "취소",
+    });
 
-  textareaContainer.appendChild(textarea);
-  return { textareaContainer, textarea };
+    this.submitButton = createElement("button", {
+      class: "button-submit submit-disabled",
+      innerText: "등록",
+      disabled: "false",
+    });
+  }
+
+  init() {
+    this.renderCommentWrite();
+    this.addEventListenerCommentWrite();
+    return this.commentWrite;
+  }
+
+  addEventListenerCommentWrite() {
+    const { textarea, cancelButton, submitButton, buttonsContainer } = this;
+
+    textarea.addEventListener("focus", () => {
+      buttonsContainer.classList.remove("no-show");
+    });
+
+    textarea.oninput = () => {
+      textarea.style.height = "auto";
+      textarea.style.height = textarea.scrollHeight + 1 + "px";
+
+      if (textarea.value) {
+        this.activeSubmitButton(true);
+      } else {
+        this.activeSubmitButton(false);
+      }
+    };
+
+    cancelButton.addEventListener("click", () => {
+      textarea.value = "";
+      buttonsContainer.classList.add("no-show");
+      this.activeSubmitButton(false);
+    });
+
+    submitButton.addEventListener("click", () => {
+      const nickName = getElement(LoginForm, "nicknameInput").value;
+      const passWord = getElement(LoginForm, "passWordInput").value;
+      const commentsUl = getElement(CommentList, "commentsList");
+      const content = textarea.value;
+
+      if (!nickName || !passWord) {
+        return alert("닉네임 또는 비밀번호를 입력해주세요!");
+      }
+
+      createComment(commentsUl, nickName, content);
+
+      nicknameInput.value = "";
+      passWordInput.value = "";
+
+      textarea.value = "";
+      textarea.style.height = "auto";
+      this.activeSubmitButton(false);
+    });
+  }
+  renderCommentWrite() {
+    const {
+      commentWrite,
+      textareaContainer,
+      textarea,
+      cancelButton,
+      submitButton,
+      buttonsContainer,
+    } = this;
+
+    textareaContainer.append(textarea);
+    buttonsContainer.append(cancelButton, submitButton);
+    commentWrite.append(textareaContainer, buttonsContainer);
+  }
+
+  activeSubmitButton(active) {
+    const { submitButton } = this;
+    if (active) {
+      submitButton.classList.remove("submit-disabled");
+      submitButton.classList.add("submit-active");
+      submitButton.disabled = false;
+    } else {
+      submitButton.classList.remove("submit-active");
+      submitButton.classList.add("submit-disabled");
+      submitButton.disabled = true;
+    }
+  }
 }
 
-// 취소, 등록 버튼 생성
-function createButtonsComment() {
-  const buttonsContainer = createElement("div", {
-    class: "buttons-container no-show",
-  });
-  const cancelButton = createElement("button", {
-    class: "button-cancel",
-    innerText: "취소",
-  });
+class CommentContainer {
+  constructor() {
+    this.commentContainer = createElement("section", { class: "comments" });
 
-  const submitButton = createElement("button", {
-    class: "button-submit submit-disabled",
-    innerText: "등록",
-    disabled: "false",
-  });
-  buttonsContainer.append(cancelButton, submitButton);
-  return { buttonsContainer, cancelButton, submitButton };
+    this.LoginForm = new LoginForm().init();
+    this.CommentWrite = new CommentWrite().init();
+    this.CommentList = new CommentList().init();
+  }
+
+  init() {
+    this.renderCommentContainer();
+    return this.commentContainer;
+  }
+
+  renderCommentContainer() {
+    const { commentContainer, LoginForm, CommentWrite, CommentList } = this;
+
+    commentContainer.append(LoginForm, CommentWrite, CommentList);
+  }
 }
 
-// 댓글 생성
-function createComment(nickname, content) {
-  const comment = createElement("li", { class: "comment" });
-  const userNickname = createElement("div", {
-    class: "nickname",
-    innerText: "@" + nickname,
-  });
-  const commentContent = createElement("p", {
-    class: "content",
-    innerText: content,
-  });
-
-  comment.append(userNickname, commentContent);
-  commentsList.appendChild(comment);
-  commentsContainer.appendChild(commentsList);
-}
-
-export default commentsContainer;
+export default new CommentContainer().init();
