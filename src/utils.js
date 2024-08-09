4cf1666bd787c20a39c40165a8c30f6c401fd082
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-08-09 15:54:15 +0900

    feat: 댓글생성 로직추가

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+// Element 생성
 export function createElement(tag, attr) {
   /** @type {Element|null} */
   const element = document.createElement(tag);
@@ -15,6 +16,28 @@ export function createElement(tag, attr) {
   }
 
   return element;
+}
+
+// 댓글생성 로직
+export function createComment(container, nickname, content) {
+  const comment = createElement("li", { class: "comment" });
+  const userNickname = createElement("div", { class: "nickname" });
+  const commentContent = createElement("p", { class: "content" });
+
+  userNickname.innerText = nickname;
+  commentContent.innerText = content;
+
+  comment.append(userNickname, commentContent);
+  container.appendChild(comment);
+}
+
+// class 모듈 내에 Element select
+export function getElement(module, element) {
+  /** @type {class} */
+  const classModule = new module();
+  /** @type {Element|null} */
+  const selectElement = classModule[element];
+  return document.querySelector(`.${selectElement.className}`);
 }
 
 // 아이콘 ClassName 리턴함수
