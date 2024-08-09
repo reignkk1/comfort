12c9e28037251a187a7f21a843c29ba7d7a7cc08
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-08-09 15:59:54 +0900

    refactor: App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { createElement } from "./utils.js";
-import commentsContainer from "./Comments.js";
+import commentContainer from "./Comments.js";
 import audioScreen from "./Audio.js";
 import "./reset.css";
 import "./styles.css";
 
 const app = createElement("main", { class: "main-content" });
 
-app.append(audioScreen, commentsContainer);
+app.append(audioScreen, commentContainer);
 
 export default app;
